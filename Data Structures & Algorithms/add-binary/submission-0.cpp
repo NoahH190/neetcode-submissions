044b7eff5919fb48bd54aba4c55cd56bb9927e5d
Noah Harman <NoahHarman190@gmail.com>
2026-08-21 13:45:30 -0400

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/add-binary/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans = ""; 
+        int i = a.size() -1;
+        int j = b.size() -1; 
+        int c = 0; 
+        while(i >= 0 || j >= 0 || c == 1) {
+            if (i >= 0){
+                c += a[i] - '0'; 
+                i--; 
+            }
+            if (j >= 0){
+                c += b[j] - '0';
+                j--;
+            }
+            ans += c%2 + '0';
+            c /= 2;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
